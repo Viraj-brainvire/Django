ed3e16e7a6f066fcc5d99a7e6622bf886de6f7e1
Viraj-Chauhan <viraj.chauhan@brainvire.com>
2024-05-01 18:43:07 +0530

    Model renew

--- MyBooks/bookApp/models.py
+++ MyBooks/bookApp/models.py
@@ -1,46 +1,39 @@
 from django.db import models
-from django.core.validators import RegexValidator
-from datetime import date
 
-# Create your models here.
-
-class Users(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-
-    def __str__(self):
-        return str(self.name)
-
-class Authors(models.Model):
-    first_name = models.CharField(max_length=254)
-    last_name = models.CharField(max_length=254)
-    address = models.CharField(max_length=254)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)
-    auth = models.ForeignKey("self", on_delete=models.CASCADE)
-    follower = models.ForeignKey(Users,on_delete=models.CASCADE)
-
-class Publishers(models.Model):
-    first_name = models.CharField(max_length=254)
-    last_name = models.CharField(max_length=254)
-    join_date = models.DateField(default=date.today)
-    popularity_score = models.DecimalField(decimal_places=3 ,max_digits=10)
-    publisher = models.ForeignKey("self", on_delete=models.CASCADE)
+class Author(models.Model):
+  firstname = models.CharField(max_length=100)
+  lastname = models.CharField(max_length=100)
+  address = models.CharField(max_length=200, null=True)
+  zipcode = models.IntegerField(null=True)
+  telephone = models.CharField(max_length=100, null=True)
+  recommendedby = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='recommended_authors', related_query_name='recommended_authors', null=True)
+  joindate = models.DateField()
+  popularity_score = models.IntegerField()
+  followers = models.ManyToManyField('User', related_name='followed_authors', related_query_name='followed_authors')
+  def __str__(self):
+    return self.firstname + ' ' + self.lastname
 
 class Books(models.Model):
-    author = models.OneToOneField(Authors,
-        on_delete=models.PROTECT,
-        blank=False)
-    
-    publisher = models.ForeignKey(Publishers,on_delete=models.PROTECT,
-        blank=False)
+  title = models.CharField(max_length=100)
+  genre = models.CharField(max_length=200)
+  price = models.IntegerField(null=True)
+  published_date = models.DateField()
+  author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books', related_query_name='books')
+  publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE, related_name='books', related_query_name='books')
+  def __str__(self):
+    return self.title
 
-    title = models.CharField(max_length=50)
-    genre = models.CharField(max_length=100)
-    price = models.DecimalField(decimal_places=3,max_digits=12)
-    publshed_date = models.DateField(default=date.today)
+class Publisher(models.Model):
+  firstname = models.CharField(max_length=100)
+  lastname = models.CharField(max_length=100)
+  recommendedby = models.ForeignKey('Publisher', on_delete=models.CASCADE, null=True)
+  joindate = models.DateField()
+  popularity_score = models.IntegerField()
+  def __str__(self):
+    return self.firstname + ' ' + self.lastname
 
-
-
-
-    
+class User(models.Model):
+  username = models.CharField(max_length=100)
+  email = models.CharField(max_length=100)
+  def __str__(self):
+    return self.username
